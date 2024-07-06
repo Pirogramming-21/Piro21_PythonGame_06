a4b417b5b14269ad3f4bd184912339fed320640d
shwn <zxz8673@gmail.com>
2024-07-06 18:49:05 +0900

    [ADD] 3. 본인의 주량 선택하기

--- main.py
+++ main.py
@@ -24,3 +24,29 @@ while True:
 
 ### 2. 사용자 이름 받기 ###
 name = input('당신의 이름은?: ')
+
+### 3. 본인 주량 선택하기 ###
+print('~~~~~~~~~~~~~~~~ 🍺 소주 기준 당신의 주량은? 🍺 ~~~~~~~~~~~~~~~~~~')
+print('''
+                    🍺 1. 소주 반병 (2잔)
+                    🍺 2. 소주 반병에서 한병 (4잔)
+                    🍺 3. 소주 한병에서 한병 반 (6잔)
+                    🍺 4. 소주 한병 반에서 두병 (8잔)
+                    🍺 5. 소주 두병 이상 (10잔)
+      
+''')
+line_print()
+
+while True:
+    try:
+        max_drink_choice =int(input('당신의 치사량(주량)은 얼마인가요? (1~5를 선택해주세요): '))
+        if max_drink_choice in range(1,6):
+            break
+        else:
+             print('1~5를 선택해주세요.')
+    except ValueError:
+        print('정수를 입력해주세요.')
+
+max_drink = max_drink_choice * 2
+print(max_drink)
+
